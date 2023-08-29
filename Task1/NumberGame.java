90bab3ad6dccfd9c9d10ff2db038a6211f2c7b7f
Faizan12456 <143254929+Faizan12456@users.noreply.github.com>
2023-08-30 00:39:05 +0530

    Add files via upload

---
+++ Task1/NumberGame.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        boolean playAgain = true;
+
+        while (playAgain) {
+            int lowerBound = 1;
+            int upperBound = 100;
+            int numberToGuess = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+
+            int maxAttempts = 12;
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+
+            System.out.println("Welcome to the Number Guessing Game!");
+            System.out.println("I have selected a random number between " + lowerBound + " and " + upperBound + ".");
+
+            while (!guessedCorrectly && attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == numberToGuess) {
+                    System.out.println("Congratulations! You guessed the correct number " + numberToGuess + " in "
+                            + attempts + " attempts.");
+                    guessedCorrectly = true;
+                } else if (userGuess < numberToGuess) {
+                    System.out.println("Your guess is too low.");
+                } else {
+                    System.out.println("Your guess is too high.");
+                }
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("Sorry, you've reached the maximum number of attempts. The correct number was: "
+                        + numberToGuess);
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainInput = scanner.next();
+            playAgain = playAgainInput.equalsIgnoreCase("yes");
+        }
+
+        System.out.println("Thank you for playing the Number Game!");
+        scanner.close();
+    }
+}
